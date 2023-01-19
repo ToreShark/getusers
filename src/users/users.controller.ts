3be3311581cf0bfeea37f000ccbe198d27c243b6
Torekhan Mukhtarov <torekhanmukhtarov@MacBook-Air-Torekhan.local>
2023-01-19 23:05:17 +0600

    #2 добавил модули afterinsert, afterremove, afterupdate

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,7 +5,7 @@ import { UsersService } from './users.service';
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
-  
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
     return this.usersService.create(body.phone, body.password);
