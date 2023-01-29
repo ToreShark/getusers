9926968b5737af15be1a52ff2a1cdbcaf6b5abd5
Torekhan Mukhtarov <torekhanmukhtarov@MacBook-Air-Torekhan.local>
2023-01-29 11:13:36 +0500

    #23 test working

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,24 +1,88 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
-it('can create an instance of auth service', async () => {
-  const fakeUsersService = {
-    find: () => Promise.resolve([]),
-    create: (phone: string, password: string) =>
-      Promise.resolve({ id: 1, phone, password }),
-  };
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      {
-        provide: UsersService,
-        useValue: fakeUsersService,
+describe('AuthService', () => {
+  let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
+
+  beforeEach(async () => {
+    const users: User[] = [];
+    fakeUsersService = {
+      find: (phone: string) => {
+        const filteredUsers = users.filter((user) => user.phone === phone);
+        return Promise.resolve(filteredUsers);
       },
-    ],
-  }).compile();
+      create: (phone: string, password: string) => {
+        const user = {
+          id: Math.floor(Math.random() * 999999),
+          phone,
+          password,
+        } as User;
+        users.push(user);
+        return Promise.resolve(user);
+      },
+    };
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
 
-  const service = module.get(AuthService);
+    service = module.get(AuthService);
+  });
 
-  expect(service).toBeDefined();
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+
+  it('create a new user with a salted and hashed password', async () => {
+    const user = await service.signup('87073816081', 'qwerty');
+
+    expect(user.password).not.toEqual('qwerty');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with phone that is in use', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([
+        {
+          id: 1,
+          phone: '87073816081',
+          password: 'asdf',
+        } as User,
+      ]);
+    await expect(service.signup('87073816081', 'qwerty')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+
+  it('throws if signin is called with an unused phone', async () => {
+    await expect(service.signin('87073816081', 'asdf')).rejects.toThrow(
+      NotFoundException,
+    );
+  });
+
+  it('throws if an invalid password is provided', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([{ phone: '87073816081', password: 'asdf' } as User]);
+    await expect(service.signin('87027568921', 'password')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+
+  it('returns a user if correct password is provided', async () => {
+    await service.signup('87073816081', 'mypassword');
+
+    const user = await service.signin('87073816081', 'mypassword');
+    expect(user).toBeDefined();
+  });
 });
